4304e21e9ede640732974dfd7c108b6d42dc7b21
Mithra K <133190992+mithjk2020@users.noreply.github.com>
2024-08-02 17:45:27 +0530

    Update steghide.sh

--- steghide.sh
+++ steghide.sh
@@ -1,4 +1,4 @@
-#save and execute this script in the same directory where mystery.jpeg is stored
+#save and execute this script in the same dir where mystery.jpeg is stored
 
 sudo apt-get install steghide
 sudo apt-get install libimage-exiftool-perl
@@ -9,12 +9,10 @@ encoded_text=$(echo -n "$input_text" | base64)
 echo "$encoded_text" > secret.txt
 
 steghide embed -ef secret.txt -cf mystery.jpeg
-#enter the passphrase here as secretsecret
+
 
 #clue in metadata
 exiftool -UserComment="Passphrase is secretsecret" mystery.jpeg
 
-#for verification
 steghide extract -sf mystery.jpeg
 
-#use 'exiftool mystery.jpeg' to read metadata
